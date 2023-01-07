bd4765e49d49a3d1683d322e93935b7c19420fe6
akankshapansare <akankshapansare@gmail.com>
2023-01-07 15:18:14 -0800

    Integrate with openai create image api

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,31 @@
-export default function Login() {
-    return (<div></div>)
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+
+export async function getStaticProps() {
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const openaiapp = new OpenAIApi(configuration);
+  const response = await openaiapp.createImage({
+    prompt:
+      "anime two littile sisters with pink and yellow dress playing in woods",
+    n: 1,
+    size: "1024x1024",
+  });
+  const imageUrl = response.data.data[0].url;
+  return {
+    props: { imageUrl },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 30 seconds
+    revalidate: 30, // In seconds };
+  };
+}
+
+export default function Login({ imageUrl }) {
+  return (
+    <div>
+      <img src={imageUrl}></img>
+    </div>
+  );
 }
